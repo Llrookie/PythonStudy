fc4cb4d274b1e2f9245604a708f9aa6f9eec4f4c
Rookie <792932348@qq.com>
2022-11-16 22:54:15 +0800

    封装补充

--- KnowledgePack/K_objectFeatures/objectTest.py
+++ KnowledgePack/K_objectFeatures/objectTest.py
@@ -1,9 +1,12 @@
 class Test1:
     def __init__(self):
         print('this is __init__')
-        self.name = 'Tom'
+        self.name = 'Tom'   # init初始化变量后，以这里为准，类内的变量将会被覆盖
         self.__age = 18
         self.age = 20
+
+    sex = 'man'
+    name = 'Mary'
 
     def __del__(self):
         print('this is __def__')
@@ -21,6 +24,8 @@ if __name__ == '__main__':
     test1 = Test1()
     test1.sleep()
     print(test1.age)  # age可在类外使用
+    print(test1.sex)    # man
+    print(test1.name)   # Tom,而不是Mary
     # print(test1.__age)  # 该输出是错误的，__age是私有变量，只能在类内部调用
 
 
